3fb694cb9e86272f7d1da6b4624a889c17d4327a
YauhenHorbach <basketman17@gmail.com>
2016-11-22 19:21:01 +0300

    Changed example.

--- generator-second-generator/app/index.js
+++ generator-second-generator/app/index.js
@@ -1,8 +1,6 @@
 const yeoman = require('yeoman-generator');
 const yosay = require('yosay');
 const camelCase = require('lodash/camelCase');
-const fs = require('fs');
-const StringDecoder = require('string_decoder').StringDecoder;
 
 module.exports = yeoman.Base.extend({
     constructor: function () {
@@ -11,7 +9,7 @@ module.exports = yeoman.Base.extend({
         this.argument('projectName', { type: String, required: false });
         this.projectName = camelCase(this.projectName);
 
-        this.option('serious', { alias: 's', type: Boolean });
+        this.option('serious', { alias: 's', type: Boolean }); // TODO doInstallLodash
         this.isSerious = this.options.serious;
         this.log(this.isSerious);
     },
@@ -20,15 +18,8 @@ module.exports = yeoman.Base.extend({
             this.log('####### Initializing task has started! #######');
         },
         work: function() {
-            try {
-                const decoder = new StringDecoder('utf8');
-                const buffer = fs.readFileSync('package.json');
-
-                this.log(decoder.write(buffer));
-            }
-            catch (err) {
-                this.log('package.json not found');
-            }
+            this.username = this.config.get('username');
+            this.log(this.username);
         },
         end: function() {
             this.log('####### Initializing task has finished! #######\n');
@@ -38,40 +29,43 @@ module.exports = yeoman.Base.extend({
         const self = this;
         self.log('####### Prompting task has started! #######');
 
-        return self.prompt([
+        const questions = [
             {
                 type    : 'input',
                 name    : 'projectName',
                 message : 'Your project name',
-                default : this.appname // Default to current folder name
+                default : self.projectName || self.appname // Default to current folder name
             },
             {
                 type    : 'confirm',
-                name    : 'cool',
-                message : 'Would you like to enable the Cool feature?'
-            },{
+                name    : 'doInstallLodash',
+                message : 'Would you like to install lodash?'
+            }
+        ];
+
+        if (!self.username) {
+            questions.push({
                 type    : 'input',
                 name    : 'username',
                 message : 'What\'s your Github username?',
                 store   : true
-            }
-        ]).then(function (answers) {
-            self.projectName = answers.projectName;
-            self.cool = answers.cool;
-            self.username = answers.username;
+            });
+        }
 
-            self.log(answers.projectName);
-            self.log(answers.cool);
-            self.log(answers.username);
-            self.log('####### Prompting task has finished! #######\n');
-        });
+        return self.prompt(questions)
+            .then(function (answers) {
+                self.projectName = answers.projectName;
+                self.doInstallLodash = answers.doInstallLodash;
+                self.username = answers.username;
+
+                self.log('####### Prompting task has finished! #######\n');
+            });
     },
     configuring: function () {
         const self = this;
 
         self.log('####### Configuring task has started! #######');
 
-        self.config.set('projectName', self.projectName);
         self.config.set('username', self.username);
 
         self.log('####### Configuring task has finished! #######\n');
@@ -91,8 +85,8 @@ module.exports = yeoman.Base.extend({
         this.log(this.templatePath('index.html'));
         this.fs.copyTpl(
             this.templatePath('index.html'),
-            this.destinationPath('public/index.html'),
-            { title: 'Templating with Yeoman' }
+            this.destinationPath(`${this.projectName}/index.html`),
+            { title: `Hey, ${this.username}` }
         );
 
         this.log('####### Writing task has finished! #######\n');
@@ -100,7 +94,12 @@ module.exports = yeoman.Base.extend({
     install: function () {
         const self = this;
         self.log('####### Install task has started! #######');
-        self.npmInstall(['lodash'], { 'save': true }, () => self.log('####### Install task has finished! #######\n'));
+        if (self.doInstallLodash) {
+            self.npmInstall(['lodash'], { 'save': true }, () => self.log('####### Install task has finished! #######\n'));
+        } else {
+            self.log('####### Lodash have not been installed! #######');
+            self.log('####### Install task has finished! #######\n');
+        }
     },
     end: function () {
         this.log('####### End task has started! #######');
